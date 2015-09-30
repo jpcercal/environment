58e3912a4e88c2306666ca922625c032c5dc5926
João Paulo Cercal <jpcercal@gmail.com>
2015-09-30 10:33:31 -0300

    The use statement StaticReview\Issue\Issue was removed

--- bin/git/hooks/pre-commit.php
+++ bin/git/hooks/pre-commit.php
@@ -18,7 +18,6 @@ EOT;
 require $autoload;
 
 use League\CLImate\CLImate;
-use StaticReview\Issue\Issue;
 use StaticReview\Reporter\Reporter;
 use StaticReview\Review\Composer\ComposerLintReview;
 use StaticReview\Review\Composer\ComposerSecurityReview;
